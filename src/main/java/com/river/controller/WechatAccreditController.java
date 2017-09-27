265e045c8cb6c2e4ee0474370eae2815e55e51c3
river <fengheatu@193.com>
2017-09-27 08:24:32 +0800

    add logback

--- src/main/java/com/river/controller/WechatAccreditController.java
+++ src/main/java/com/river/controller/WechatAccreditController.java
@@ -2,6 +2,8 @@ package com.river.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import com.river.util.AuthUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,11 +15,17 @@ import java.net.URLEncoder;
  * desc:
  */
 @Controller
-public class WechatController {
+public class WechatAccreditController {
 
+    private static final Logger logger = LoggerFactory.getLogger(WechatAccreditController.class);
 
+    /**
+     * 微信授权登录第一步，跳转授权页面，
+     * @return
+     */
     @RequestMapping("login")
     public String wechatTest() {
+
         String backUrl = "http://c32f415f.ngrok.io/wxAuth/callBack";
         String url = "https://open.weixin.qq.com/connect/oauth2/authorize?"
                 + "appid=" + AuthUtil.APPID
@@ -25,13 +33,18 @@ public class WechatController {
                 + "&response_type=code"
                 + "&scope=SCOPE"
                 + "&state=STATE#wechat_redirect";
-
         return "redirect:" + url;
     }
 
-
+    /**
+     * 微信授权毁回调地址
+     * @param code
+     * @throws IOException
+     */
     @RequestMapping("/wxAuth/callBack")
     public void callBack(String code) throws IOException {
+
+        //获取access_token openid
         String url = " https://api.weixin.qq.com/sns/oauth2/access_token?" +
                 "appid=" + AuthUtil.APPID +
                 "&secret=" + AuthUtil.APPSECRET +
@@ -43,7 +56,7 @@ public class WechatController {
         String openId = jsonObject.getString("openid");
         System.out.println(jsonObject.toJSONString());
 
-
+        //获取用户信息
         String infoUrl = "https://api.weixin.qq.com/sns/userinfo?" +
                 "access_token=" + accessToken +
                 "&openid=" + openId +
--- src/main/java/com/river/controller/WechatAuthTokenController.java
+++ src/main/java/com/river/controller/WechatAuthTokenController.java
@@ -1,6 +1,8 @@
 package com.river.controller;
 
 import com.river.util.SHA1;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,16 +12,27 @@ import java.util.Arrays;
 
 @Controller
 @RequestMapping("/auth")
-public class WechatAuthToken {
+public class WechatAuthTokenController {
+
+	private static final Logger logger = LoggerFactory.getLogger(WechatAuthTokenController.class);
+
 	// 自定义 token
 	private static final String TOKEN = "river";
 
-
+	/**
+	 * 微信token认证
+	 * @param signature
+	 * @param echostr
+	 * @param timestamp
+	 * @param nonce
+	 * @return
+	 * @throws IOException
+	 */
 	@RequestMapping("/token")
 	@ResponseBody
 	public String authToken(String signature ,String echostr , String timestamp ,String nonce)
 			throws  IOException {
-
+		logger.info("开始token认证");
 		String[] str = { TOKEN, timestamp, nonce };
 		Arrays.sort(str); // 字典序排序
 		String bigStr = str[0] + str[1] + str[2];
